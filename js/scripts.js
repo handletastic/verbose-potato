c84b05f931fece6d2f37fd25660fe79e1f765815
johannes muljana <jmuljana@gmail.com>
2016-05-13 00:26:44 +0000

    updated scripts.js to handle login

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@
 var spinner = '<i class="fa fa-spinner fa-spin spinner"></i>';
 //alert for forms
 var formalert = '<div class="alert alert-dismissible">'+
-'<span class="message"></span>'
+'<span class="message"></span>'+
 '<button type="button" class="close" data-dismiss="alert">'+
 '<span>&times;</span></button></div>';
 //submit variable to stop double clicking
@@ -42,47 +42,54 @@ $(document).ready(function(){
                //add alert to the form
                showMessage("#user-registration",true,"your account has been created,why not go shopping?");
                //wait 10 seconds and remove the message
-               setTimeout(function(){removeMessage("#user-registration")},10000);
+               //setTimeout(function(){removeMessage("#user-registration")},5000);
             }
             else if(data.errors){
-               var message;
-               //use error codes from user-registration.php
-               switch(data.errors.code){
-                  case 1:
-                     message = "tokens not matching";
-                     break;
-                  case 2:
-                     message = "that email address is already used, would you like to"+"<a href='password-reset.php'>"+" reset"+"</a> the password instead?";
-                     break;
-                  case 3:
-                     message = "password needs to be 8 characters or more";
-                     break;
-                  case 4:
-                     message = "email address is invalid";
-                     break;
-                  case 5:
-                     message = "no form data received";
-                     break;
-                  case 6:
-                     message = "sorry, account creation failed";
-                     break;
-                  default:
-                     break;
-               }
+               message = showErrorCode(data.errors.code);
                showMessage("#user-registration",false,message);
             }
-         })
-         .fail(function(errors){
-            //do something if fails
-            console.log(errors);
          });
       }
-   })
+   });
+   // ------------------------------------------user login form
+   $("#user-login").on("submit",function(event){
+      event.preventDefault();
+      var formData = {
+         'email'     : $('input[name=email]').val(),
+         'password'  : $('input[name=password]').val(),
+         'user-token': $('input[name=user-token]').val()
+      };
+      if(submitting==false){
+         addSpinner(event);
+         submitting=true;
+         $.ajax({
+            type        : 'POST',
+            url         : 'ajax/user-login.php',
+            data        : formData,
+            dataType    : 'json',
+            encode      : true
+         })
+         .done(function(data){
+            removeSpinner(event);
+            submitting = false;
+            if(data.success){
+               //add alert to the form
+               showMessage("#user-login",true,"welcome back!");
+               
+            }
+            else if(data.errors){
+               message = showErrorCode(data.errors.code);
+               showMessage("#user-login",false,message);
+            }
+         });
+      }
+      
+   });
 });
 
 //functions
 function addSpinner(event){
-   $(event.target).find(".btn-register").append(spinner);
+   $(event.target).find(".btn").append(spinner);
 }
 
 function removeSpinner(event){
@@ -101,7 +108,46 @@ function showMessage(element,success,message){
 }
 
 function removeMessage(element){
-   var target = $(element).parent().find(".alert");
-   $(target).addClass("close");
-   //$(element).find(".message").remove();
+   var target = $(element).parents().find(".alert");
+   $(target).addClass("close").on("transitionend",function(){
+      $(target).remove();
+   });
+}
+
+function showErrorCode(code){
+   //use error codes from user-registration.php
+   switch(code){
+      case 1:
+         message = "tokens not matching";
+         return message;
+         break;
+      case 2:
+         message = "that email address is already used, would you like to"+"<a href='password-reset.php'>"+" reset"+"</a> the password instead?";
+         return message;
+         break;
+      case 3:
+         message = "password needs to be 8 characters or more";
+         return message;
+         break;
+      case 4:
+         message = "email address is invalid";
+         return message;
+         break;
+      case 5:
+         message = "no form data received";
+         return message;
+         break;
+      case 6:
+         message = "sorry, account creation failed";
+         return message;
+         break;
+      case 7:
+         message = "authentication failed";
+         return message;
+      case 8:
+         message = "your account has been deactivated";
+         return message;
+      default:
+         break;
+   }
 }
